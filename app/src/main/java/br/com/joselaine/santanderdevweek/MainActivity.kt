2758904e91556e2bf93e562edc0ddc8274e2815d
Joselaine Aparecida dos Santos <joselaine.aparecida@gmail.com>
2021-05-26 16:26:46 -0300

    layout finalizado

--- app/src/main/java/br/com/joselaine/santanderdevweek/MainActivity.kt
+++ app/src/main/java/br/com/joselaine/santanderdevweek/MainActivity.kt
@@ -2,10 +2,24 @@ package br.com.joselaine.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayShowTitleEnabled(false);
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
 }
