5e5b2015a71331aa025af3ecc89526e258cfb7af
Joselaine Aparecida dos Santos <joselaine.aparecida@gmail.com>
2021-05-28 15:53:18 -0300

    revert

--- app/src/main/java/br/com/joselaine/santanderdevweek/ui/MainActivity.kt
+++ app/src/main/java/br/com/joselaine/santanderdevweek/ui/MainActivity.kt
@@ -2,16 +2,12 @@ package br.com.joselaine.santanderdevweek.ui
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.joselaine.santanderdevweek.R
 import br.com.joselaine.santanderdevweek.data.Conta
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.text.NumberFormat
 import java.util.*
 
@@ -26,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         buscarContaCliente()
+
     }
 
     private fun buscarContaCliente() {
@@ -34,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun bindOnView(conta: Conta) {
+    fun bindOnView(conta: Conta) {
         val soma = conta.limite + conta.saldo
         "Olá, ${conta.cliente.nome}".also { findViewById<TextView>(R.id.tv_usuario).text = it }
         "Ag ${conta.agencia}".also { findViewById<TextView>(R.id.tv_agencia).text = it }
@@ -52,23 +49,6 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.item_1 -> {
-                MaterialAlertDialogBuilder(applicationContext,
-                    R.style.Base_ThemeOverlay_AppCompat_Dialog_Alert)
-                    .setMessage(resources.getString(R.string.long_message))
-                    .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
-                        Toast.makeText(applicationContext, "Sinto muito!", Toast.LENGTH_LONG).show()
-                    }
-                    .setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
-                        Toast.makeText(applicationContext, "Você receberá nossas novidades!", Toast.LENGTH_LONG).show()
-                    }
-                    .show()
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
 
 fun Double.formatarValor(): String {
